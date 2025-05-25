f02f6dac841e0f577ad79a213170364dcfd063b6
Moriah Ogbenna <moriahonyin@gmail.com>
2025-05-25 15:17:47 +0100

    ran some code on it

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,3 @@
-console.log(Cis fun);
-console.log(Python is cool);
-console.log(JavaScript is amazing)
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing")
